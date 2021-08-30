7da25217c5267cd449dda0c48200217dfc8d8381
Stephen Byatt <47413006+stevebyatt10@users.noreply.github.com>
2021-08-30 16:55:22 +1000

    Initial Commit

---
+++ Tennis/TennisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TennisApp.swift
+//  Tennis
+//
+//  Created by Stephen Byatt on 30/8/21.
+//
+
+import SwiftUI
+
+@main
+struct TennisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TennisTests/TennisTests.swift
@@ -0,0 +1,33 @@
+//
+//  TennisTests.swift
+//  TennisTests
+//
+//  Created by Stephen Byatt on 30/8/21.
+//
+
+import XCTest
+@testable import Tennis
+
+class TennisTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
